7b271d488b98386bca648da759ffba8969a5bc71
GabrielaDelgado <delga132@umn.edu>
2023-09-24 11:12:19 -0500

    ERD diagram

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,63 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/JNPK0Q
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "category_df" (
+    "category_id" VARCHAR(4)   NOT NULL,
+    "category" VARCHAR(15)   NOT NULL,
+    CONSTRAINT "pk_category_df" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory_df" (
+    "subcategory_id" VARCHAR(8)   NOT NULL,
+    "subcategory" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_subcategory_df" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "campaign_df" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" VARCHAR(30)   NOT NULL,
+    "description" VARCHAR(30)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" VARCHAR(30)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" VARCHAR(30)   NOT NULL,
+    "currency" VARCHAR(30)   NOT NULL,
+    "launched_date" datetime   NOT NULL,
+    "end_date" datetime   NOT NULL,
+    "category_id" VARCHAR(30)   NOT NULL,
+    "subcategory_id" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_campaign_df" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "contacts_df" (
+    "contact_id" int   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_contacts_df" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "campaign_df" ADD CONSTRAINT "fk_campaign_df_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts_df" ("contact_id");
+
+ALTER TABLE "campaign_df" ADD CONSTRAINT "fk_campaign_df_category_id" FOREIGN KEY("category_id")
+REFERENCES "category_df" ("category_id");
+
+ALTER TABLE "campaign_df" ADD CONSTRAINT "fk_campaign_df_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory_df" ("subcategory_id");
+
